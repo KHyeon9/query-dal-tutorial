7823ad549fea8d83354df38bf5ce3834a103b423
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-04-14 20:06:12 +0900

    데이터 확인을 위한 SiteUser에 ToString 추가

--- src/main/java/com/query_dsl/query_dsl_tutorial/boundedContext/user/entity/SiteUser.java
+++ src/main/java/com/query_dsl/query_dsl_tutorial/boundedContext/user/entity/SiteUser.java
@@ -1,15 +1,13 @@
 package com.query_dsl.query_dsl_tutorial.boundedContext.user.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter @Getter
+@ToString
 public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
