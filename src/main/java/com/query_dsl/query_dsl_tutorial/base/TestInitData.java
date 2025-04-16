62484a3ebe3e75490dfc5aa6c33ec196a0b2f57d
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-04-16 19:58:59 +0900

    1번 회원과 2번회원이 공통 관심사를 갖도록 테스트 데이터 추가

--- src/main/java/com/query_dsl/query_dsl_tutorial/base/TestInitData.java
+++ src/main/java/com/query_dsl/query_dsl_tutorial/base/TestInitData.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @Profile("test") // 이 클래스 안에 정의된 Bean들은 test 모드에서만 실행
@@ -25,13 +24,27 @@ public class TestInitData {
                     .email("user1@email.com")
                     .build();
 
+
+
             SiteUser user2 = SiteUser.builder()
                     .username("user2")
                     .password("{noop}1234")
                     .email("user2@email.com")
                     .build();
 
-            List<SiteUser> users = userRepository.saveAll(Arrays.asList(user1, user2));
+            userRepository.saveAll(Arrays.asList(user1, user2));
+            // 중간 테이블이 있을 때, 아래 값을 설정하고 save를 한번에 하면 중복값 저장으로 생각해 에러를 반환
+            // 즉, 회원이 있어서 keyword 생성이 가능
+            
+            user1.addInterestKeword("야구");
+            user1.addInterestKeword("농구");
+
+            user2.addInterestKeword("등산");
+            user2.addInterestKeword("캠핑");
+            user2.addInterestKeword("야구");
+
+
+            userRepository.saveAll(Arrays.asList(user1, user2));
         };
     }
 }
